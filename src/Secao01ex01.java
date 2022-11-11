f3ef113b8ea30e7801178ae8090f41dae0c8aa85
davidluiz91 <dvdluiz1991@gmail.com>
2022-11-11 16:33:17 -0300

    estrutura_condicional

---
+++ src/Secao01ex01.java
@@ -0,0 +1,23 @@
+public class Secao01ex01 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 30;
+        int soma = num1 + num2;
+
+        System.out.println(soma);
+
+        int num3 = -30;
+        int num4 = 10;
+        int soma1 = num3 + num4;
+
+        System.out.println(soma1);
+
+        int num5 = 0;
+        int num6 = 0;
+        int soma2 = num5 + num6;
+
+        System.out.println(soma2);
+
+
+    }
+}
